e4e05062d07505e035ce7316969c99b7215544a7
thanh-cao <kimthanh.cao@gmail.com>
2022-05-19 22:08:09 +0200

    add products view and create template for product listing

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Product, Category
 # Create your views here.
+
+
+def all_products(request):
+    '''
+    This view will show all products in the database including ability
+    to filter by categories and allergies
+    '''
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'products': products,
+        'categories': categories
+    }
+    return render(request, 'products/products.html', context)
--- sushiish_shop/settings.py
+++ sushiish_shop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'allauth',
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'sushiish_shop.urls'
