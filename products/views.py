9cc0964244fc9fb13e858c8c8d70ee9f07a02054
thanh-cao <kimthanh.cao@gmail.com>
2022-05-22 15:38:16 +0200

    create product detail offcanvas view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 # Create your views here.
 
@@ -15,3 +15,14 @@ def all_products(request):
         'categories': categories
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    '''
+    This view will show details of a product
+    '''
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product
+    }
+    return render(request, 'products/product_detail.html', context)
