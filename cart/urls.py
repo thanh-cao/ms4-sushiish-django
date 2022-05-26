85f5bc5088a344adba3c41b07a2023c7822f53b9
thanh-cao <kimthanh.cao@gmail.com>
2022-05-26 21:24:44 +0200

    add functionality to adjust quantity and remove item from cart

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('adjust/<product_id>/', views.adjust_cart, name='adjust_cart'),
+    path('remove/<product_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect, reverse
 from .contexts import cart_contents
 
 # Create your views here.
@@ -29,3 +29,35 @@ def add_to_cart(request, product_id):
     request.session['cart'] = cart
     return JsonResponse({'cart': request.session['cart'],
                          'order_total': order_total})
+
+
+def adjust_cart(request, product_id):
+    '''
+    This view adjusts the quantity of a product in the cart
+    '''
+    print('adjust_cart')
+    quantity = int(request.POST.get('quantity'))
+    print(quantity)
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[product_id] = quantity
+    else:
+        cart.pop(product_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, product_id):
+    '''
+    This view removes a product from the cart
+    '''
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(product_id)
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
