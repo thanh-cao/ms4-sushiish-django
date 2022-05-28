e45b9b5ecfc2c1b4199b779468dc638bb3ba7da2
thanh-cao <kimthanh.cao@gmail.com>
2022-05-29 00:10:58 +0200

    create template and form for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from checkout.forms import OrderForm
 
 # Create your views here.
+
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        return redirect('products')
+    else:
+        order_form = OrderForm()
+
+        context = {
+            'cart': cart,
+            'order_form': order_form
+        }
+        return render(request, 'checkout/checkout.html', context)
