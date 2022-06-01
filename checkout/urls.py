74dfe3fc7fa770809884392065aa497c27551ca2
thanh-cao <kimthanh.cao@gmail.com>
2022-06-01 23:00:01 +0200

    set up the basics for stripe webhook handler

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
-
+from .webhook import stripe_webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('update_order_type/', views.update_order_type, name='update_order_type'),
-    path('update_order_info/', views.update_order_info, name='update_order_info'),
+    path('update_order_type/', views.update_order_type,
+         name='update_order_type'),
+    path('update_order_info/', views.update_order_info,
+         name='update_order_info'),
     path('stripe/keys/', views.get_stripe_keys, name='get_stripe_keys'),
-    path('success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('success/<order_number>', views.checkout_success,
+         name='checkout_success'),
+    path('webhook/', stripe_webhook, name='stripe_webhook'),
 ]
---
+++ checkout/webhook_handler.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+
+
+class StripeWebhook_Handler:
+    '''
+    Stripe webhook handler
+    '''
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        '''
+        Handle a generic/unknown/unexpected webhook event
+        '''
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_success(self, event):
+        '''
+        Handle the payment_intent.succeeded webhook from Stripe
+        '''
+        print('Payment succeeded!')
+        return HttpResponse(
+            content=f'Payment succeeded',
+            status=200)
+
+    def handle_payment_failure(self, event):
+        '''
+        Handle the payment_intent.payment_failed webhook from Stripe
+        '''
+        print('Payment failed.')
+        return HttpResponse(
+            content=f'Payment failed',
+            status=200)
